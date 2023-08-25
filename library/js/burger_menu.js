25b56d4b4af6113e0ad38df24938cf3f90bd1ece
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-08-25 23:16:33 +0300

    refactor: change the structure of the code burgerMenu.js

--- library/js/burger_menu.js
+++ library/js/burger_menu.js
@@ -1,18 +1,20 @@
-
 const burgerMenu = document.querySelector('.burger-menu');
 const background = document.querySelector('.background-click');
 const burgerIcon = document.querySelectorAll('.burger-icon');
 const navItems = document.querySelectorAll('.nav-item');
 
 background.addEventListener('click', toggleBurger);
-navItems.forEach(i => {    
-    i.addEventListener('click', toggleBurger)
-});
-
-burgerIcon[0].addEventListener('click', toggleBurger);
-burgerIcon[1].addEventListener('click', toggleBurger);
 
 function toggleBurger() {
     burgerMenu.classList.toggle('opened');
     background.classList.toggle('hidden');
 }
+
+function elementsEvent(elementsArray) {
+    elementsArray.forEach(i => {    
+        i.addEventListener('click', toggleBurger)
+    });
+}
+
+elementsEvent(burgerIcon);
+elementsEvent(navItems);
