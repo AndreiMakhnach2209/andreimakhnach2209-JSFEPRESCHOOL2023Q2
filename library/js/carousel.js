3b68d37e0beba8aee33ee72ad04ec3af3752f704
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-08-26 13:30:46 +0300

    feat: add functionality of pagination buttons in the section About

--- library/js/carousel.js
+++ library/js/carousel.js
@@ -1,19 +1,38 @@
 const carouselButtonsArea = document.querySelectorAll('.carousel-button-link');
 const carouselButtons = document.querySelectorAll('.carousel-button');
-const carouselimages = document.querySelectorAll('.carousel-item');
+const carouselImages = document.querySelectorAll('.carousel-item');
+const carousel = document.querySelector('.carousel-box');
 let indexActiveButton = 0;
 
 function paging(event) {
     const target = event.target;
-    carouselButtons.forEach((item, index) =>{
+    if (target != carouselButtons[indexActiveButton]) imagesPagig();
+    buttonsPaging(target);
+} 
+
+function buttonsPaging(target) {
+    carouselButtons.forEach((item, index) => {
         if (item === target) 
             indexActiveButton = (indexActiveButton < index) ? indexActiveButton + 1
-                              : (indexActiveButton > index) ? indexActiveButton - 1
-                              : indexActiveButton
+                            : (indexActiveButton > index) ? indexActiveButton - 1
+                            : indexActiveButton
         item.classList.remove('active');
-    })
+    });
     carouselButtons[indexActiveButton].classList.add('active');
-} 
+}
+
+function imagesPagig (){
+    carousel.classList.add('opacity');
+    carousel.addEventListener('transitionend', () => {
+        carouselImages.forEach((item, index) => {
+            item.classList.add('nodisplay');
+            if (indexActiveButton  <= index && index <= indexActiveButton +2) {
+                item.classList.remove('nodisplay');
+            }
+        })
+        carousel.classList.remove('opacity')
+    });
+}
 
 function buttonActivation(elements) {
     elements.forEach((item, index) => {
