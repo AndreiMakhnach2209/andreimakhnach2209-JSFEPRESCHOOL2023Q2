fceed6a3b5edd1570563957781ad2a2e9534cca2
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-09-07 23:55:18 +0300

    feat: save user data to local storage

--- library/js/form_handler.js
+++ library/js/form_handler.js
@@ -1,6 +1,6 @@
 const formRegister = document.forms.register;
 let newUserData = {};
-let newCardNumber = '';
+let activeUser = {};
 
 function userDataReceive(form) {
     const data = new FormData(form);
@@ -14,18 +14,34 @@ function userDataReceive(form) {
 function cardNumberGenerator() {
     let numCard = '';
     for (let i = 0; i < 9; i++) {
-        const hexNum = Math.floor(Math.random() * 17)
+        const hexNum = Math.floor(Math.random() * 16)
                         .toString(16)
                         .toUpperCase();
         numCard += hexNum;
+        console.log(hexNum)
     }
     return numCard;
 }
 
-
+function userVerification (data) {
+    return !(Object.keys(localStorage).includes(data.email));
+}
 
 formRegister.addEventListener('submit', (event) => {
     event.preventDefault();
-    newUserData = userDataReceive( formRegister );
-    console.log(newUserData);
+    newUserData = userDataReceive(formRegister);
+    if (userVerification(newUserData)) {
+        do {
+            newUserData.cardNumber = cardNumberGenerator();
+        } while (Object.keys(localStorage).includes(newUserData.cardNumber));
+        newUserData.visits = 1;
+        newUserData.books = [];
+        activeUser = Object.assign({}, newUserData);
+        localStorage.setItem(newUserData.email, JSON.stringify(newUserData));
+        localStorage.setItem(newUserData.cardNumber, newUserData.email);
+        newUserData = {};
+    }else{
+        alert('This email has already been registered!');
+    }
+    console.log(newUserData , activeUser);
 })
